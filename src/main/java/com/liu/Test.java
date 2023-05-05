895dc76a2daf0b9e4e3b7225fb4aad650ab846c7
zaileng <294100345@qq.com>
2023-05-05 20:24:53 +0800

    test1 commit

--- src/main/java/com/liu/Test.java
+++ src/main/java/com/liu/Test.java
@@ -10,6 +10,5 @@ public class Test {
         System.out.println("host-fix test ");
         System.out.println("push test ");
         System.out.println("pull test ");
-        System.out.println("test1 test ");
     }
 }
